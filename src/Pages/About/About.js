4e39688b1be4a9b7f4e0d086075eedd6de2e698d
pritom48 <pritamdas41148@gmail.com>
2022-04-18 19:18:51 +0600

    blog added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import mypic from '../../img/IMG_20220307_235243_785.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>this in fancy about</h2>
+        <div className='container'>
+            <div className='d-flex '>
+                <img height={400} className="rounded mx-auto d-block" src={mypic} alt="" />
+            </div>
         </div>
     );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>this is blog</h2>
+        <div className='container'>
+            <div className='my-5'>
+                <h2>1.Difference between authorization and authentication?</h2>
+                <h4>**Authentication</h4>
+                <p>1.Authentication verifies who the user is.</p>
+                <p>2.Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.</p>
+                <p>3.Authentication is the first step of a good identity and access management process.</p>
+                <p>4.Authentication is visible to and partially changeable by the user.</p>
+                <p>5.Example: By verifying their identity, employees can gain access to an HR application that includes their personal pay information, vacation time, and 401K data.</p>
+
+                <h4>**Authorization</h4>
+                <p>1.Authorization determines what resources a user can access.</p>
+                <p>2.Authorization works through settings that are implemented and maintained by the organization.</p>
+                <p>3.Authorization always takes place after authentication.</p>
+                <p>4.Authorization isn’t visible to or changeable by the user.</p>
+                <p>5.Example: Once their level of access is authorized, employees and HR managers can access different levels of data based on the permissions set by the organization.</p>
+            </div>
+            <div className='my-5'>
+                <h2>2.What other services does firebase provide other than authentication?</h2>
+                <h4>##There are many services which Firebase provides, Most useful of them are</h4>
+                <p>1.Cloud Firestore</p>
+                <p>2.Cloud Functions</p>
+                <p>3.Hosting</p>
+                <p>4.Google Analytics</p>
+                <p>5.Dynamic Links</p>
+            </div>
         </div>
     );
 };
